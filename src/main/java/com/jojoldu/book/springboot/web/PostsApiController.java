026534b5b0d6ae0b0b65f6a63c70429728b6686a
JoshuaHong0215 <sebin5736@gmail.com>
2025-09-05 15:45:07 +0900

    진행과정

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -1,21 +1,36 @@
 package com.jojoldu.book.springboot.web;
 
-import com.jojoldu.book.springboot.sevice.PostsService;
+import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 
-@RestController
+import java.util.List;
+
 @RequiredArgsConstructor
-
+@RestController
 public class PostsApiController {
 
     private final PostsService postsService;
 
+    // 등록하기
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    // 수정기능
+    @PutMapping("/api/v1/posts/{id}")
+    public  Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    //조회하기
+    @GetMapping("/api/v1/posts/{id}")
+    public  PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
     }
 }
